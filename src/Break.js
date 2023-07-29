b2218393a7ff8fec5615e417f96d3eeafa6dd5ec
swatidurgekar <swatidurgekar@gmail.com>
2023-07-29 09:45:35 +0530

    Implementing increase and decrease for break length

--- src/Break.js
+++ src/Break.js
@@ -4,14 +4,23 @@ import { ImArrowUp2, ImArrowDown2 } from "react-icons/im";
 
 const Break = () => {
   const [breakLength, setBreakLength] = useState(5);
+
+  const increase = () => {
+    if (breakLength < 60) setBreakLength((prevLength) => prevLength + 1);
+  };
+
+  const decrease = () => {
+    if (breakLength > 1) setBreakLength((prevLength) => prevLength - 1);
+  };
+
   return (
     <div>
       <p className={classes.title}>Break Length</p>
-      <span className={classes.arrow}>
+      <span onClick={increase} className={classes.arrow}>
         <ImArrowUp2 style={{ fontSize: "20px" }} />
       </span>
       <span className={classes.breaklength}>{breakLength}</span>
-      <span className={classes.arrow}>
+      <span onClick={decrease} className={classes.arrow}>
         <ImArrowDown2 style={{ fontSize: "20px" }} />
       </span>
     </div>
